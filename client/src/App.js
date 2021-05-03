207f1cd07792c8af23bf226e43e0ba050f6c6495
dang lu <dangnlu18@gmail.com>
2021-05-02 21:25:44 -0700

    render data from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import {Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
 import Home from './routes/home'
 import RestaurantDetailPage from './routes/restaurantDetailPage'
-import UpdatePage from './routes/updatePage'
+import UpdatePage from './routes/updatePage' 
+import { RestaurantsContextProvider } from './context/RestaurantsContext'
+
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route  exact path='/restaurants/:id' component={RestaurantDetailPage} />
-      <Route  exact path='/restaurants/:id/update' component={UpdatePage} />
-    </Switch>
+    <RestaurantsContextProvider>
+      <div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route  exact path='/restaurants/:id' component={RestaurantDetailPage} />
+          <Route  exact path='/restaurants/:id/update' component={UpdatePage} />
+        </Switch>
+      </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 }
 
--- client/src/components/restaurantList.js
+++ client/src/components/restaurantList.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
+import restaurantFinder from '../apis/restaurantFinder'
+import {RestaurantsContext} from '../context/RestaurantsContext'
 
-const RestaurantList = () =>{
+const RestaurantList = (props) =>{
+    const {restaurants, setRestaurants } = useContext(RestaurantsContext)
+
+
+    useEffect( ()=>{
+
+        const fetchData = async () =>{
+            try{
+                const results = await restaurantFinder.get('/')
+                setRestaurants(results.data.data.restaurants)
+            }
+            catch(err){
+                console.log(err)
+            }
+        }
+
+        fetchData()
+    },[])
+
     return(
         <div>
             <table>
@@ -15,14 +35,18 @@ const RestaurantList = () =>{
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Taco Bell</td>
-                        <td>New York</td>
-                        <td>$$</td>
-                        <td>Rating</td>
-                        <td><button>Edit</button></td>
-                        <td><button>Delete</button></td>
-                    </tr>
+    
+                    {restaurants && restaurants.map(rest => {return(
+                        <tr key={rest.id}>
+                            <td>{rest.name}</td>
+                            <td>{rest.location}</td>
+                            <td>{"$".repeat(rest.price_range)}</td>
+                            <td>reviews</td>
+                            <td><button>Edit</button></td>
+                            <td><button>Delete</button></td>
+                        </tr>
+                    )
+                    })}
                 </tbody>
             </table>
         </div>
