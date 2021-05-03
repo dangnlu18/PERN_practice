79948944f61ae4a9743a042bd26cd4ced05feb86
dang lu <dangnlu18@gmail.com>
2021-05-03 15:49:14 -0700

    details page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const db = require('./db/index.js')
 const morgan = require('morgan')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+
 
 
 app.get('/api/v1/restaurants', async (req,res)=>{
